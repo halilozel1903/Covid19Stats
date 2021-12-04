2df142a7b58e401b3f1b7a8e028b793b073d1074
Halil ÖZEL <halilozel1903@gmail.com>
2021-12-04 23:44:26 +0300

    Model classes was updated.

--- app/src/main/java/com/halil/ozel/covid19stats/data/CountriesResponse.kt
+++ app/src/main/java/com/halil/ozel/covid19stats/data/CountriesResponse.kt
@@ -4,10 +4,6 @@ class CountriesResponse {
     var country: String? = null
     var recovered: String? = null
     var cases: String? = null
-    var critical: String? = null
-    var deathsPerOneMillion: String? = null
-    var active: String? = null
-    var casesPerOneMillion: String? = null
     var countryInfo: CountryInfo? = null
     var deaths: String? = null
     var todayCases: String? = null
--- app/src/main/java/com/halil/ozel/covid19stats/data/CountryInfo.kt
+++ app/src/main/java/com/halil/ozel/covid19stats/data/CountryInfo.kt
@@ -2,14 +2,4 @@ package com.halil.ozel.covid19stats.data
 
 class CountryInfo {
     var flag: String? = null
-    private var _id: String? = null
-    var iso2: String? = null
-    var iso3: String? = null
-    fun get_id(): String? {
-        return _id
-    }
-
-    fun set_id(_id: String?) {
-        this._id = _id
-    }
 }

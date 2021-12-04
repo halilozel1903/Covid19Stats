3c398d1285b5376bea75ebca3e7f4b5d1ca6633c
Halil ÖZEL <halilozel1903@gmail.com>
2021-12-04 23:47:38 +0300

    API End Point was updated.

--- app/src/main/java/com/halil/ozel/covid19stats/api/CoronaApi.kt
+++ app/src/main/java/com/halil/ozel/covid19stats/api/CoronaApi.kt
@@ -6,14 +6,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 object CoronaApi {
     private var retrofit: Retrofit? = null
     private const val BASE_URL = "https://corona.lmao.ninja/v2/"
+
     @JvmStatic
     val retrofitInstance: Retrofit?
         get() {
             if (retrofit == null) {
                 retrofit = Retrofit.Builder()
-                        .baseUrl(BASE_URL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
             }
             return retrofit
         }
--- app/src/main/java/com/halil/ozel/covid19stats/api/CoronaService.kt
+++ app/src/main/java/com/halil/ozel/covid19stats/api/CoronaService.kt
@@ -1,20 +1,18 @@
 package com.halil.ozel.covid19stats.api
 
-import com.halil.ozel.covid19stats.data.AllCountriesResponse
 import com.halil.ozel.covid19stats.data.CountriesResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CoronaService {
-    @get:GET("countries/?sort=country")
-    val countries: Call<List<CountriesResponse>>
+
+    @GET("countries/?sort=country")
+    fun getCountryList():Call<List<CountriesResponse>>
 
     @GET("countries/{country}")
     fun getCountryInfo(
-            @Path("country") country: String?
+        @Path("country") country: String?
     ): Call<CountriesResponse?>?
 
-    @get:GET("all")
-    val allCountries: Call<AllCountriesResponse?>?
 }
